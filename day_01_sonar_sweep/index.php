2ad723d28852508818037e78c6adb3724e1c3a50
Timo Schinkel <timo.schinkel@coolblue.nl>
2021-12-02 12:03:00 +0100

    Remove output from VCS
    Remove output files from VCS and rename to `output_part_1` in preparation of part 2.

--- day_01_sonar_sweep/index.php
+++ day_01_sonar_sweep/index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 $contents = explode(PHP_EOL, file_get_contents(__DIR__ . '/input.txt'));
 file_put_contents(__DIR__ . '/output.txt', '');
+file_put_contents(__DIR__ . '/output_part_1.txt', '');
 
 $previous = null;
 $numOfIncreases = 0;
@@ -11,12 +12,12 @@ $numOfIncreases = 0;
 foreach ($contents as $depth) {
     $depth = intval(trim($depth));
     if ($previous === null) {
-        file_put_contents(__DIR__ . '/output.txt', sprintf("%d\t%s\n", $depth, '(N/A - no previous measurement)'), FILE_APPEND);
+        file_put_contents(__DIR__ . '/output_part_1.txt', sprintf("%d\t%s\n", $depth, '(N/A - no previous measurement)'), FILE_APPEND);
     } elseif ($depth > $previous) {
         $numOfIncreases++; // money shot!
-        file_put_contents(__DIR__ . '/output.txt', sprintf("%d\t%s\n", $depth, '(increased)'), FILE_APPEND);
+        file_put_contents(__DIR__ . '/output_part_1.txt', sprintf("%d\t%s\n", $depth, '(increased)'), FILE_APPEND);
     } else {
-        file_put_contents(__DIR__ . '/output.txt', sprintf("%d\t%s\n", $depth, '(decreased)'), FILE_APPEND);
+        file_put_contents(__DIR__ . '/output_part_1.txt', sprintf("%d\t%s\n", $depth, '(decreased)'), FILE_APPEND);
     }
 
     $previous = $depth;
