04c6bd1161fb82df77fac5f1c0f96411e3d3ec3f
Timo Schinkel <timo.schinkel@coolblue.nl>
2021-12-02 13:14:39 +0100

    Day 01: Part 2

--- day_01_sonar_sweep/index.php
+++ day_01_sonar_sweep/index.php
@@ -2,15 +2,15 @@
 
 declare(strict_types=1);
 
-$contents = explode(PHP_EOL, file_get_contents(__DIR__ . '/input.txt'));
-file_put_contents(__DIR__ . '/output.txt', '');
+$measurements = array_map('intval', explode(PHP_EOL, trim(file_get_contents(__DIR__ . '/input.txt'))));
 file_put_contents(__DIR__ . '/output_part_1.txt', '');
+file_put_contents(__DIR__ . '/output_part_2.txt', '');
 
+// Part 1
 $previous = null;
 $numOfIncreases = 0;
 
-foreach ($contents as $depth) {
-    $depth = intval(trim($depth));
+foreach ($measurements as $depth) {
     if ($previous === null) {
         file_put_contents(__DIR__ . '/output_part_1.txt', sprintf("%d\t%s\n", $depth, '(N/A - no previous measurement)'), FILE_APPEND);
     } elseif ($depth > $previous) {
@@ -24,3 +24,46 @@ foreach ($contents as $depth) {
 }
 
 echo "Number of times a depth measurement increases: " . $numOfIncreases . PHP_EOL;
+
+// Part 2: Sliding window
+$previous = null;
+$numOfIncreases = 0;
+
+for ($i = 0; $i < count($measurements) - 2; $i++) {
+    $window = Window::create($measurements, $i);
+
+    if ($previous === null) {
+        file_put_contents(__DIR__ . '/output_part_2.txt', sprintf("%s\t%d\t%s\n", chr(65 + $i), $window->getValue(), '(N/A - no previous sum)'), FILE_APPEND);
+    } elseif ($window->getValue() > $previous->getValue()) {
+        $numOfIncreases++; // money shot!
+        file_put_contents(__DIR__ . '/output_part_2.txt', sprintf("%s\t%d\t%s\n", chr(65 + $i), $window->getValue(), '(increased)'), FILE_APPEND);
+    } elseif ($window->getValue() === $previous->getValue()) {
+        file_put_contents(__DIR__ . '/output_part_2.txt', sprintf("%s\t%d\t%s\n", chr(65 + $i), $window->getValue(), '(no change)'), FILE_APPEND);
+    } else {
+        file_put_contents(__DIR__ . '/output_part_2.txt', sprintf("%s\t%d\t%s\n", chr(65 + $i), $window->getValue(), '(decreased)'), FILE_APPEND);
+    }
+
+    $previous = $window;
+}
+
+echo "Number of sums larger than the previous sum: " . $numOfIncreases . PHP_EOL;
+
+final class Window
+{
+    private int $value;
+
+    public function __construct(int $value)
+    {
+        $this->value = $value;
+    }
+
+    public static function create(array $measurements, int $start): self
+    {
+        return new self(array_sum(array_slice($measurements, $start, 3)));
+    }
+
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+}
