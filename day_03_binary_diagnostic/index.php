6d94a5f6edbba444c0827053be01df465a6754bf
Timo Schinkel <timo.schinkel@coolblue.nl>
2021-12-03 08:54:32 +0100

    Day 03: Part 2

--- day_03_binary_diagnostic/index.php
+++ day_03_binary_diagnostic/index.php
@@ -32,3 +32,66 @@ for($position = 0; $position < strlen($numbers[0]); $position++) {
 
 echo 'What is the power consumption of the submarine?' . PHP_EOL;
 echo (bindec($gamma) * bindec($epsilon)) . PHP_EOL;
+
+// Part 2
+
+$oxygen_generator_rating = '';
+
+$working_copy = $numbers;
+for($position = 0; $position < strlen($numbers[0]); $position++) {
+    $ones = [];
+    $zeros = [];
+
+    foreach ($working_copy as $number) {
+        if ($number[$position] === '1') {
+            $ones[] = $number;
+        } else {
+            $zeros[] = $number;
+        }
+    }
+
+    if (count($ones) >= count($zeros)) {
+        $oxygen_generator_rating .= '1';
+        $working_copy = $ones;
+    } else {
+        $oxygen_generator_rating .= '0';
+        $working_copy = $zeros;
+    }
+
+    if (count($working_copy) === 1) {
+        $oxygen_generator_rating = reset($working_copy);
+        break;
+    }
+}
+
+$co2_scrubber_rating = '';
+
+$working_copy = $numbers;
+for($position = 0; $position < strlen($numbers[0]); $position++) {
+    $ones = [];
+    $zeros = [];
+
+    foreach ($working_copy as $number) {
+        if ($number[$position] === '1') {
+            $ones[] = $number;
+        } else {
+            $zeros[] = $number;
+        }
+    }
+
+    if (count($ones) >= count($zeros)) {
+        $co2_scrubber_rating .= '0';
+        $working_copy = $zeros;
+    } else {
+        $co2_scrubber_rating .= '1';
+        $working_copy = $ones;
+    }
+
+    if (count($working_copy) === 1) {
+        $co2_scrubber_rating = reset($working_copy);
+        break;
+    }
+}
+
+echo 'What is the life support rating of the submarine?' . PHP_EOL;
+echo (bindec($oxygen_generator_rating) * bindec($co2_scrubber_rating)) . PHP_EOL;
