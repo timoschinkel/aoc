e3445e9b0295d29c973e301847b0635daeb0997b
Timo Schinkel <timo.schinkel@coolblue.nl>
2021-12-07 10:06:39 +0100

    Day 07: part 2
    I am not mathematical enough to find a single formula for the second part of the assignment. It probably is something like a weighted median or something. Was simpler to brute force it :shrug:.

--- day_07_treachery_of_whales/index.php
+++ day_07_treachery_of_whales/index.php
@@ -16,3 +16,26 @@ $fuel_consumption = array_reduce($positions, fn(int $carry, int $item): int => $
 
 echo 'How much fuel must they spend to align to that position?' . PHP_EOL;
 echo $fuel_consumption . PHP_EOL;
+
+// Part 2:
+$min = array_reduce($positions, fn(int $carry, int $item): int => min($carry, $item), reset($positions));
+$max = array_reduce($positions, fn(int $carry, int $item): int => max($carry, $item), 0);
+
+$consumptions = array_fill(0, $max + 1, 0);
+for ($horizontal_position = $min; $horizontal_position <= $max; $horizontal_position++) {
+    // iterate over all possible horizontal positions
+    $consumptions[$horizontal_position] = array_reduce(
+        $positions,
+        function(int $carry, int $crab_position) use ($horizontal_position): int {
+            $n = (int)abs($horizontal_position - $crab_position);
+
+            return $carry + ($n * ($n + 1) / 2);
+        },
+        0
+    );
+}
+
+asort($consumptions, SORT_ASC);
+
+echo 'How much fuel must they spend to align to that position?' . PHP_EOL;
+echo reset($consumptions) . PHP_EOL;
