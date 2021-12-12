d07b424ab09089f8ad90d82db401cf5760bdd9ef
Timo Schinkel <timo.schinkel@coolblue.nl>
2021-12-12 10:55:46 +0100

    Day 12: part 2
    I think there is room for optimization with the process of determining if there was a small cave visited twice in the past.

--- day_12_passage_pathing/index.php
+++ day_12_passage_pathing/index.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 $inputs = explode(PHP_EOL, trim(file_get_contents(__DIR__ . '/input.txt')));
 
-// Part 1
 $graph = Graph::create($inputs);
+
+// Part 1
 $num_of_paths = $graph->findNumberOfPaths('start', 'end');
 
 echo 'How many paths through this cave system are there that visit small caves at most once?' . PHP_EOL;
 echo $num_of_paths . PHP_EOL;
+
+// Part 2
+$num_of_paths_2 = $graph->findNumberOfPaths2('start', 'end');
+
+echo 'How many paths through this cave system are there that visit small caves at most once?' . PHP_EOL;
+echo $num_of_paths_2 . PHP_EOL;
 
 final class Graph
 {
@@ -38,9 +45,12 @@ final class Graph
     public function findNumberOfPaths(string $start, string $end): int
     {
         $paths = $this->findPaths($start, $end, [$start]);
+        return count($paths);
+    }
 
-//        die(var_dump(__CLASS__, __LINE__, join(PHP_EOL, array_map(fn(array $path): string => join(',', $path), $paths))));
-
+    public function findNumberOfPaths2(string $start, string $end): int
+    {
+        $paths = $this->findPaths2($start, $end, [$start]);
         return count($paths);
     }
 
@@ -70,8 +80,50 @@ final class Graph
         return $paths;
     }
 
+    /**
+     * @param string $start
+     * @param string $end
+     * @param array<string> $path_so_far
+     * @return array<array<string>>
+     */
+    private function findPaths2(string $start, string $end, array $path_so_far = []): array
+    {
+        if ($start === $end) {
+            return [$path_so_far];
+        }
+
+        // try all possible steps and add them to all paths
+        $paths = [];
+        foreach ($this->edges[$start] ?? [] as $node) {
+            if ($this->canVisitCave($path_so_far, $node) === false) {
+                // we have already visited the small cave, making all $paths invalid
+                continue;
+            }
+
+            $paths = array_merge($paths, $this->findPaths2($node, $end, array_merge($path_so_far, [$node])));
+        }
+
+        return $paths;
+    }
+
     private function isSmallCave(string $cave): bool
     {
         return strtolower($cave) === $cave;
+    }
+
+    private function canVisitCave(array $path_so_far, string $cave): bool
+    {
+        if ($this->isSmallCave($cave) === false || !in_array($cave, $path_so_far)) {
+            // if big cave or non-visited small cave, we can visit:
+            return true;
+        }
+
+        if (in_array($cave, ['start', 'end']) && in_array($cave, $path_so_far)) {
+            // exception: start and end can only be visited once
+            return false;
+        }
+
+        $small_cave_visits = array_count_values(array_values(array_filter($path_so_far, fn(string $node): bool => $this->isSmallCave($node))));
+        return count(array_filter($small_cave_visits, fn(int $visits): bool => $visits >= 2)) === 0;
     }
 }
