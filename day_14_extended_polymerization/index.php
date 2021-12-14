356db49057c18fdfb23eb6437338a2febf844b20
Timo Schinkel <timo.schinkel@coolblue.nl>
2021-12-14 09:49:05 +0100

    Day 14: part 2
    Similar apprach as day 6, but now with the extra handicap of having overlapping pairs. Thanks to GolezTrol for the inspiration.

--- day_14_extended_polymerization/index.php
+++ day_14_extended_polymerization/index.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 
 $inputs = explode(PHP_EOL, trim(file_get_contents(__DIR__ . '/input.txt')));
 
-// Part 1
 $formula = Polymer::create($inputs);
+
+// Part 1
 $score = $formula->getScoreAfterSteps(10);
+
+echo 'What do you get if you take the quantity of the most common element and subtract the quantity of the least common element?' . PHP_EOL;
+echo $score . PHP_EOL . PHP_EOL;
+
+// Part 2
+$score = $formula->getScoreAfterStepsOptimized(40);
 
 echo 'What do you get if you take the quantity of the most common element and subtract the quantity of the least common element?' . PHP_EOL;
 echo $score . PHP_EOL;
@@ -81,6 +88,48 @@ final class Polymer
         }
 
         return $new . $polymer[strlen($polymer) - 1];
+    }
+
+    public function getScoreAfterStepsOptimized(int $num_of_steps): int
+    {
+        $pairs = array_combine(
+            array_keys($this->rules),
+            array_fill(0, count($this->rules), 0)
+        );
+
+        for($i = 0; $i < strlen($this->template) - 1; $i++) {
+            $chunk = substr($this->template, $i, 2);
+            $pairs[$chunk]++;
+        }
+
+        for ($step = 0; $step < $num_of_steps; $step++) {
+            // iterate over all pairs and apply the rule
+            $eligible = array_filter($pairs);
+            foreach ($eligible as $pattern => $occurrences) {
+                $rule = $this->rules[$pattern];
+
+                $pairs[$pattern] -= $occurrences;
+                $pairs[$pattern[0] . $rule->getInsertion()] += $occurrences;
+                $pairs[$rule->getInsertion() . $pattern[1]] += $occurrences;
+            }
+        }
+
+        ksort($pairs);
+
+        // Determine score
+        $element_count = [];
+        foreach ($pairs as $pattern => $occurrences) {
+            // Because every pair overlaps, only take the first character of every pair
+            $element_count[$pattern[0]] = ($element_count[$pattern[0]] ?? 0) + $occurrences;
+        }
+
+        // The last element of the template is never counted in the previous step
+        // as it will always be the second part of a pair. But it will also never
+        // change:
+        $element_count[$this->template[strlen($this->template) - 1]] += 1;
+
+        asort($element_count);
+        return end($element_count) - reset($element_count);
     }
 
     public static function create(array $inputs): self
