2ddb2d3e8adc3ff10877535e3f0d565dbb067f8a
Timo Schinkel <timo.schinkel@coolblue.nl>
2022-12-01 08:31:18 +0100

    [Day 01] Part 1

--- 2022/Day01/Program.cs
+++ 2022/Day01/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var answer = 1234;
 
-Console.WriteLine("Hello, World!");
+string[] input = System.IO.File.ReadAllLines(@"./input.txt");
+Console.WriteLine($"Number of lines: {input.Length}");
+
+int max = 0;
+int current = 0;
+foreach (var line in input)
+{
+    if (line == "")
+    {
+        max = Math.Max(current, max);
+        current = 0;
+        continue;
+    }
+
+    current += Int32.Parse(line);
+}
+
+max = Math.Max(current, max);
+
+Console.WriteLine($"How many total Calories is that Elf carrying? {max}");
