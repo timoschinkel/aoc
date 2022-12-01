46869bccd55573c42d8d9fa0990370a253d523f0
Timo Schinkel <timo.schinkel@coolblue.nl>
2022-12-01 08:44:07 +0100

    [Day 01] Part 2

--- 2022/Day01/Program.cs
+++ 2022/Day01/Program.cs
@@ -1,14 +1,18 @@
-﻿var answer = 1234;
-
-string[] input = System.IO.File.ReadAllLines(@"./input.txt");
+﻿string[] input = File.ReadAllLines(@"./input.txt");
 Console.WriteLine($"Number of lines: {input.Length}");
 
 int max = 0;
 int current = 0;
+List<int> elves = new List<int>();
+
 foreach (var line in input)
 {
     if (line == "")
     {
+        if (current > 0)
+        {
+            elves.Add(current);
+        }
         max = Math.Max(current, max);
         current = 0;
         continue;
@@ -18,5 +22,11 @@ foreach (var line in input)
 }
 
 max = Math.Max(current, max);
+elves.Add(current);
 
 Console.WriteLine($"How many total Calories is that Elf carrying? {max}");
+
+elves = elves.OrderByDescending(value => value).ToList();
+int total = elves.GetRange(0, 3).Sum();
+
+Console.WriteLine($"How many Calories are those Elves carrying in total? {total}");
