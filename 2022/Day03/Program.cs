064867152fe8d908f4bb831778857fed67f34abd
Timo Schinkel <timo.schinkel@coolblue.nl>
2022-12-03 10:55:30 +0100

    [Day 03] Part 1

---
+++ 2022/Day03/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] input = File.ReadAllLines(@"./input.txt");
+
+var score = 0;
+foreach (var line in input)
+{
+    var length = line.Length;
+    var one = line.Substring(0, length / 2);
+    var two = line.Substring(length / 2);
+
+    int c = one.ToCharArray().Intersect(two.ToCharArray()).ToArray()[0];
+    score += c >= 97 ? /* lowercase */ c - 96 : /* uppercase */ c - 38;
+}
+
+Console.WriteLine($"What is the sum of the priorities of those item types? {score}");
