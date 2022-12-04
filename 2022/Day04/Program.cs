02d69451f7bc03d674a0196bba6019795eba7ffe
Timo Schinkel <timo.schinkel@coolblue.nl>
2022-12-04 10:16:24 +0100

    [Day 04] Part 1

---
+++ 2022/Day04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+string[] input = File.ReadAllLines(@"./input.txt");
+
+var score = 0;
+
+Regex rx = new Regex(@"^([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)$");
+
+foreach (var line in input)
+{
+    var match = rx.Matches(line).First();
+    var one = Enumerable.Range(Int32.Parse(match.Groups[1].Value), Int32.Parse(match.Groups[2].Value) - Int32.Parse(match.Groups[1].Value) + 1);
+    var two = Enumerable.Range(Int32.Parse(match.Groups[3].Value), Int32.Parse(match.Groups[4].Value) - Int32.Parse(match.Groups[3].Value) + 1);
+
+    if (one.Except(two).Count() == 0 || two.Except(one).Count() == 0)
+    {
+        score++;
+    }
+}
+
+Console.WriteLine($"In how many assignment pairs does one range fully contain the other? {score}");
