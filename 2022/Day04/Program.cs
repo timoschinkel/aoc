94c3a597f75278e8c93ec45f38b3e1867e466139
Timo Schinkel <timo.schinkel@coolblue.nl>
2022-12-04 10:29:44 +0100

    [Day 04] Part 2

--- 2022/Day04/Program.cs
+++ 2022/Day04/Program.cs
@@ -3,6 +3,7 @@
 string[] input = File.ReadAllLines(@"./input.txt");
 
 var score = 0;
+var scoreTwo = 0;
 
 Regex rx = new Regex(@"^([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)$");
 
@@ -12,10 +13,16 @@ foreach (var line in input)
     var one = Enumerable.Range(Int32.Parse(match.Groups[1].Value), Int32.Parse(match.Groups[2].Value) - Int32.Parse(match.Groups[1].Value) + 1);
     var two = Enumerable.Range(Int32.Parse(match.Groups[3].Value), Int32.Parse(match.Groups[4].Value) - Int32.Parse(match.Groups[3].Value) + 1);
 
-    if (one.Except(two).Count() == 0 || two.Except(one).Count() == 0)
+    if (one.Except(two).Count() < one.Count() || two.Except(one).Count() < two.Count())
     {
-        score++;
+        if (one.Except(two).Count() == 0 || two.Except(one).Count() == 0)
+        {
+            score++;
+        }
+
+        scoreTwo++;
     }
 }
 
 Console.WriteLine($"In how many assignment pairs does one range fully contain the other? {score}");
+Console.WriteLine($"In how many assignment pairs do the ranges overlap? {scoreTwo}");
