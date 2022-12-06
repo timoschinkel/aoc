be150af07b5baa53163844ef9d2f671fa2ad3850
Timo Schinkel <timo.schinkel@coolblue.nl>
2022-12-06 08:33:04 +0100

    [Day 06] Part 1

---
+++ 2022/Day06/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] input = File.ReadAllLines(@"./input.txt");
+
+foreach (var line in input)
+{
+    for (var i = 0; i < line.Length - 4; i++)
+    {
+        if (line.Substring(i, 4).ToCharArray().Distinct().Count() == 4)
+        {
+            // unique sequence found!
+            Console.WriteLine($"How many characters need to be processed before the first start-of-packet marker is detected? {i + 4}");
+            break;
+        }
+    }
+}
