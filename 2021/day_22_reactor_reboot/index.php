91d43209962ededc54c7e5fd90a2f7849eff9a33
Timo Schinkel <timo.schinkel@coolblue.nl>
2023-01-18 15:56:47 +0100

    [2021] Day 22 - Optimization
    After implementing part 2 I realized that it is probably much more efficient to apply that methodology to part 1 as well. That resulted in a reduction of execution time from almost 3 seconds to below 10ms.
    
    Surprisingly it also reduced the execution time of part 2. Possibly due to either caching of less memory consumption.

--- 2021/day_22_reactor_reboot/index.php
+++ 2021/day_22_reactor_reboot/index.php
@@ -8,9 +8,10 @@ $inputs = explode(PHP_EOL, trim(file_get_contents(__DIR__ . '/' . ($argv[1] ?? '
 $expection = $argv[2] ?? null;
 
 // Part 1
+$steps = array_filter(array_map(fn(string $input): ?LimitedCuboid => LimitedCuboid::create($input), $inputs));
+
 $stopwatch = hrtime(true);
-$steps = array_filter(array_map(fn(string $input): ?Step => Step::create($input), $inputs));
-$result = (new Grid())->countCubesThatAreOn(...$steps);
+$result = calculateNumberOfCubes($steps);
 $elapsedMilliseconds = round((hrtime(true) - $stopwatch)/1e+6, 0);
 
 echo "How many cubes are on? {$result} ({$elapsedMilliseconds}ms)" . PHP_EOL;
@@ -22,163 +23,72 @@ echo "How many cubes are on? {$result} ({$elapsedMilliseconds}ms)" . PHP_EOL;
 // end of this operation we are able to calculate the volume of the cuboids in the final list and that should tell us
 // exactly how many cubes are on.
 $cuboids = array_filter(array_map(fn(string $input): ?Cuboid => Cuboid::create($input), $inputs));
-//$cuboids = array_slice($cuboids, 0, 2);
 
-/** @var Cuboid[] $final */
-$final = []; // cuboids that don't intersect
-
-$cuboids = array_reverse($cuboids); // Stack is LIFO
-
-while (count($cuboids) > 0) {
-    /** @var Cuboid $current */
-    $current = array_pop($cuboids);
-
-    // Keep a list of items to remove from `$final` and a list of items to add. This prevents that we need to change
-    // the order or $final while looping over it.
-    $indicesToRemoveFromFinal = [];
-    $cuboidsToAddToFinal = [];
-
-    foreach ($final as $index => $cuboid) {
-        if ($cuboid->intersects($current)) {
-            // handle intersection
-            if ($current->on()) {
-                // operation of $current is on, so we need to split up $current over all the intersecting points. Every
-                // resulting cuboids is to be checked for intersections. Easiest way to do this is by putting the split up
-                // cuboids back into $cuboids to be handled by a next iteration. There is one special case; If $current is
-                // completely covered by the cuboid that intersects it. If this is the other way around for now it is easier to
-                // still split up and put the non-intersecting cuboids on $stack. This is due to the fact that the operation is
-                // in order, so we would have to rerun the entire operation again.
-                $sections = $current->diff($cuboid);
-
-                $cuboids = array_merge($cuboids, $sections);
-                continue 2; // continue to next on stack
-            } else {
-                // operation of $current is off, so we need to split up the intersected cuboids in $final, remove the
-                // intersecting cuboids and put the non-intersecting cuboids back in $final
-                $sections = $cuboid->diff($current);
-
-                $indicesToRemoveFromFinal[] = $index;
-                $cuboidsToAddToFinal = array_merge($cuboidsToAddToFinal, $sections);
-            }
-        }
-    }
-
-    // $current does not intersect, if the operation is "on" we can add it to $final
-    if ($current->on()) {
-        $final[] = $current;
-    }
-
-    foreach (array_reverse($indicesToRemoveFromFinal) as $index_to_remove) {
-        unset($final[$index_to_remove]);
-    }
-    $final = array_values(array_merge($final, $cuboidsToAddToFinal));
-}
-
-$total = 0;
-foreach($final as $cuboid) {
-    $total += $cuboid->volume();
-}
-
+$total = calculateNumberOfCubes($cuboids);
 $elapsedMilliseconds = round((hrtime(true) - $stopwatch)/1e+6, 0);
 echo "How many cubes are on? {$total} ({$elapsedMilliseconds}ms)" . PHP_EOL;
 
-final class Step
+function calculateNumberOfCubes(array $cuboids): int
 {
-    private string $operation;
-    private int $xs;
-    private int $xt;
-    private int $ys;
-    private int $yt;
-    private int $zs;
-    private int $zt;
+    /** @var Cuboid[] $final */
+    $final = []; // cuboids that don't intersect
 
-    public function __construct(string $operation, int $xs, int $xt, int $ys, int $yt, int $zs, int $zt)
-    {
-        $this->operation = $operation;
-        $this->xs = max(-50, min(50, $xs));
-        $this->xt = max(-50, min(50, $xt));
-        $this->ys = max(-50, min(50, $ys));
-        $this->yt = max(-50, min(50, $yt));
-        $this->zs = max(-50, min(50, $zs));
-        $this->zt = max(-50, min(50, $zt));
-    }
+    $cuboids = array_reverse($cuboids); // Stack is LIFO
 
-    public function isOn(): bool
-    {
-        return $this->operation === 'on';
-    }
+    while (count($cuboids) > 0) {
+        /** @var Cuboid $current */
+        $current = array_pop($cuboids);
 
-    public function x(): array
-    {
-        return range($this->xs, $this->xt, 1);
-    }
+        // Keep a list of items to remove from `$final` and a list of items to add. This prevents that we need to change
+        // the order or $final while looping over it.
+        $indicesToRemoveFromFinal = [];
+        $cuboidsToAddToFinal = [];
 
-    public function y(): array
-    {
-        return range($this->ys, $this->yt, 1);
-    }
+        foreach ($final as $index => $cuboid) {
+            if ($cuboid->intersects($current)) {
+                // handle intersection
+                if ($current->on()) {
+                    // operation of $current is on, so we need to split up $current over all the intersecting points. Every
+                    // resulting cuboids is to be checked for intersections. Easiest way to do this is by putting the split up
+                    // cuboids back into $cuboids to be handled by a next iteration. There is one special case; If $current is
+                    // completely covered by the cuboid that intersects it. If this is the other way around for now it is easier to
+                    // still split up and put the non-intersecting cuboids on $stack. This is due to the fact that the operation is
+                    // in order, so we would have to rerun the entire operation again.
+                    $sections = $current->diff($cuboid);
 
-    public function z(): array
-    {
-        return range($this->zs, $this->zt, 1);
-    }
+                    $cuboids = array_merge($cuboids, $sections);
+                    continue 2; // continue to next on stack
+                } else {
+                    // operation of $current is off, so we need to split up the intersected cuboids in $final, remove the
+                    // intersecting cuboids and put the non-intersecting cuboids back in $final
+                    $sections = $cuboid->diff($current);
 
-    private static function isInRange(int $value, int $min = -50, int $max = 50): bool
-    {
-        return $value >= $min && $value <= $max;
-    }
-
-    public static function create (string $input): ?self
-    {
-        preg_match('%^(?P<operation>on|off) x=(?P<x_from>-?\d+)\.\.(?P<x_to>-?\d+),y=(?P<y_from>-?\d+)\.\.(?P<y_to>-?\d+),z=(?P<z_from>-?\d+)\.\.(?P<z_to>-?\d+)$%si', $input, $matches);
-
-        // If one of the axis is completely out of range, the entire Step is out of range:
-        if ((!self::isInRange((int)$matches['x_from']) && !self::isInRange((int)$matches['x_to'])) ||
-            (!self::isInRange((int)$matches['y_from']) && !self::isInRange((int)$matches['y_to'])) ||
-            (!self::isInRange((int)$matches['z_from']) && !self::isInRange((int)$matches['z_to']))) {
-            return null;
-        }
-
-        return new self(
-            $matches['operation'],
-            (int)$matches['x_from'], (int)$matches['x_to'],
-            (int)$matches['y_from'], (int)$matches['y_to'],
-            (int)$matches['z_from'], (int)$matches['z_to']
-        );
-    }
-}
-
-final class Grid
-{
-    public function countCubesThatAreOn(Step ...$steps): int
-    {
-        $on = [];
-
-        foreach ($steps as $i => $step) {
-            foreach ($step->x() as $x) {
-                foreach ($step->y() as $y) {
-                    foreach ($step->z() as $z) {
-                        if ($x < -50 || $x > 50 || $y < -50 && $y > 50 || $z < -50 || $z > 50) {
-                            // out of bounds
-                            continue;
-                        }
-
-                        $point = json_encode(['x' => $x, 'y' => $y, 'z' => $z]);
-                        $on[$point] = $step->isOn();
-                    }
+                    $indicesToRemoveFromFinal[] = $index;
+                    $cuboidsToAddToFinal = array_merge($cuboidsToAddToFinal, $sections);
                 }
             }
-
-            $on = array_filter($on);
-
-            //echo 'After step ' . ($i + 1) . ' - ' . $step . ': ' . count($on) . PHP_EOL;
         }
 
-        return count($on);
+        // $current does not intersect, if the operation is "on" we can add it to $final
+        if ($current->on()) {
+            $final[] = $current;
+        }
+
+        foreach (array_reverse($indicesToRemoveFromFinal) as $index_to_remove) {
+            unset($final[$index_to_remove]);
+        }
+        $final = array_values(array_merge($final, $cuboidsToAddToFinal));
     }
+
+    $total = 0;
+    foreach($final as $cuboid) {
+        $total += $cuboid->volume();
+    }
+
+    return $total;
 }
 
-final class Cuboid
+class Cuboid
 {
     private bool $on;
     private int $x1;
@@ -347,5 +257,31 @@ final class Cuboid
     public function toString(): string
     {
         return sprintf('%s x=%d..%d, y=%d..%d, z=%d..%d (%d)', $this->on ? 'on' : 'off', $this->x1, $this->x2, $this->y1, $this->y2, $this->z1, $this->z2, $this->volume());
+    }
+}
+
+final class LimitedCuboid extends Cuboid
+{
+    private static function isInRange(int $value): bool
+    {
+        return $value >= -50 && $value <= 50;
+    }
+    public static function create (string $input): ?self
+    {
+        preg_match('%^(?P<operation>on|off) x=(?P<x_from>-?\d+)\.\.(?P<x_to>-?\d+),y=(?P<y_from>-?\d+)\.\.(?P<y_to>-?\d+),z=(?P<z_from>-?\d+)\.\.(?P<z_to>-?\d+)$%si', $input, $matches);
+
+        // If one of the axis is completely out of range, the entire Step is out of range:
+        if ((!self::isInRange((int)$matches['x_from']) && !self::isInRange((int)$matches['x_to'])) ||
+            (!self::isInRange((int)$matches['y_from']) && !self::isInRange((int)$matches['y_to'])) ||
+            (!self::isInRange((int)$matches['z_from']) && !self::isInRange((int)$matches['z_to']))) {
+            return null;
+        }
+
+        return new self(
+            $matches['operation'] === 'on',
+            max(-50, min(50, (int)$matches['x_from'])), max(-50, min(50, (int)$matches['x_to'])),
+            max(-50, min(50, (int)$matches['y_from'])), max(-50, min(50, (int)$matches['y_to'])),
+            max(-50, min(50, (int)$matches['z_from'])), max(-50, min(50, (int)$matches['z_to']))
+        );
     }
 }
