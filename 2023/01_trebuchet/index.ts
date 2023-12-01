06ffd26415b6392c3e4e1a22030c6285b5f6e335
Timo Schinkel <timo.schinkel@coolblue.nl>
2023-12-01 09:06:36 +0100

    [2023] Day 01, part 2

--- 2023/01_trebuchet/index.ts
+++ 2023/01_trebuchet/index.ts
@@ -4,6 +4,7 @@ const input = readFileSync(`${__dirname}/${process.argv[2] || 'example'}.txt`, {
 const debug: boolean = false;
 
 console.log('What is the sum of all of the calibration values?', part_one(input));
+console.log('What is the sum of all of the calibration values?', part_two(input));
 
 function part_one(input: string[]): number {
     let sum = 0;
@@ -18,8 +19,35 @@ function part_one(input: string[]): number {
     return sum;
 }
 
+function part_two(input: string[]): number {
+    // The difficulty here is that eigh2two3three should be converted to 82233,
+    // so the textual representation "eight" can be split by multiple numeric chars.
+    // Let's do regular expressions!
+
+    const converted = input.map(line => {
+        const c = Object.entries({
+            '(z[0-9]*e[0-9]*r[0-9]*o)': '0$1',
+            '(o[0-9]*n[0-9]*e)': '1$1',
+            '(t[0-9]*w[0-9]*o)': '2$1',
+            '(t[0-9]*h[0-9]*r[0-9]*e[0-9]*e)': '3$1',
+            '(f[0-9]*o[0-9]*u[0-9]*r)': '4$1',
+            '(f[0-9]*i[0-9]*v[0-9]*e)': '5$1',
+            '(s[0-9]*i[0-9]*x)': '6$1',
+            '(s[0-9]*e[0-9]*v[0-9]*e[0-9]*n)': '7$1',
+            '(e[0-9]*i[0-9]*g[0-9]*h[0-9]*t)': '8$1',
+            '(n[0-9]*i[0-9]*n[0-9]*e)': '9$1',
+        }).reduce((carry: string, [search, replace]) => {
+            return carry.replaceAll(new RegExp(search, 'gsi'), replace);
+        }, line);
+        log(line, c);
+        return c;
+    });
+
+    return part_one(converted);
+}
+
 function log(...args: unknown[]): void {
     if (debug) {
-        console.log(args);
+        console.log(...args);
     }
 }
