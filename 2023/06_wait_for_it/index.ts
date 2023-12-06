071d52dd454151193c583a2d595d9358be8a8109
Timo Schinkel <timo.schinkel@coolblue.nl>
2023-12-06 14:47:08 +0100

    [2023] Day 06, part 2, very optimized (< 1ms)

--- 2023/06_wait_for_it/index.ts
+++ 2023/06_wait_for_it/index.ts
@@ -17,6 +17,11 @@ const race = parse_single(input);
     console.log('How many ways can you beat the record in this one much longer race?', part_two(race))
 }
 
+{
+    using sw = new Stopwatch('part two optimized');
+    console.log('How many ways can you beat the record in this one much longer race?', part_two_optimized(race))
+}
+
 function part_one(input: Race[]): number {
     /*
      * There is probably a very smart way to limit the possible press times, but just
@@ -64,6 +69,51 @@ function part_two(game: Race): number {
     }
 
     return upper_boundary - lower_boundary + 1;
+}
+
+function part_two_optimized(game: Race): number {
+    /**
+     * After giving this problem a bit more thought, and after reading up on some
+     * discussions on Reddit there is a another solution that relies on mathematics
+     * rather than computer science.
+     *
+     * We can write our problem as follows:
+     * We have T milliseconds, we can press for p milliseconds, and we can then determine
+     * the D distance. In a formula this can be written as: D = p * (T - p); the distance
+     * is equal to the time pressed times time left (= total time T minus pressed P).
+     *
+     * We need to find a solution for P where the following formula is valid:
+     * d = p * (T - p) where  d > D
+     *
+     * For the example this would become
+     * p * (71530 - p) > 940200
+     *
+     * Through algebra this can be rewritten:
+     * p * 71530 - p * p > 940200   -- rewrite
+     * 71530p - p^2 > 940200        -- divide both sides by p^2
+     * 71530p > p^2 + 940200        -- extract 71530p from both sides
+     * 0 > p^2 - 71530p + 940200
+     *
+     * Now we have reduced the problem to a quadratic formula - ax^2 + bx +c -,
+     * and for quadratic formulas we have a standardized way of finding solutions:
+     *
+     * x = (-b +/- sqrt(b^2 - 4ac)) / 2a
+     *
+     * Using this formula we can find the two values of x - or in our problem p -
+     * where the formula equals to 0. Between those values are the values for p where
+     * the distance d will exceed distance D.
+     *
+     * For our puzzle a = 1, b = T = total time, and c = D = distance to beat
+     */
+
+    const lower = (-game.time - Math.sqrt(Math.pow(game.time, 2) - 4 * game.distance)) / 2;
+    const upper = (-game.time + Math.sqrt(Math.pow(game.time, 2) - 4 * game.distance)) / 2;
+
+    /*
+     * We are only concerned in whole numbers, so we need to take the ceiling of the lower bound,
+     * the floor of the upper bound and subtract 1, because we want the number of items.
+     */
+    return Math.abs(Math.ceil(lower) - Math.floor(upper) - 1);
 }
 
 type Race = {
