c1afebb7f2954c2f5e740313720d6ec3e4907961
Timo Schinkel <timo.schinkel@coolblue.nl>
2023-12-07 15:15:50 +0100

    [2023] Day 07, part 2

--- 2023/07_camel_cards/index.ts
+++ 2023/07_camel_cards/index.ts
@@ -4,6 +4,16 @@ import { Stopwatch } from '../stopwatch';
 const input = readFileSync(`${__dirname}/${process.argv[2] || 'example'}.txt`, { encoding:'utf8', flag:'r' }).trim().split("\n").map(line => line.trim());
 const debug: boolean = !!(process.env.DEBUG || false);
 
+const Type = {
+    FIVE_OF_A_KIND: 6,
+    FOUR_OF_A_KIND: 5,
+    FULL_HOUSE: 4,
+    THREE_OF_A_KIND: 3,
+    TWO_PAIR: 2,
+    ONE_PAIR: 1,
+    HIGH_CARD: 0,
+};
+
 const card_ranks = {
     '2': 2,
     '3': 3,
@@ -15,6 +25,22 @@ const card_ranks = {
     '9': 9,
     'T': 10,
     'J': 11,
+    'Q': 12,
+    'K': 13,
+    'A': 14,
+}
+
+const card_ranks_wildcard = {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'T': 10,
+    'J': 0,
     'Q': 12,
     'K': 13,
     'A': 14,
@@ -45,6 +71,25 @@ class Game {
         // they're the same
         return 0;
     }
+
+    public compareToPartTwo(another: Game): number {
+        const r1 = rank_wildcard(this);
+        const r2 = rank_wildcard(another);
+
+        if (r1 != r2) {
+            return r1 - r2;
+        }
+
+        // same type of hand, compare the cards from the left to the right
+        for (let i = 0; i < this.cards.length; i++) {
+            if (this.cards[i] !== another.cards[i]) {
+                return card_ranks_wildcard[this.cards[i]] - card_ranks_wildcard[another.cards[i]];
+            }
+        }
+
+        // they're the same
+        return 0;
+    }
 }
 
 const games = parse(input);
@@ -53,6 +98,12 @@ const games = parse(input);
     using sw = new Stopwatch('part one');
     console.log('What are the total winnings?', part_one(games));
 }
+
+{
+    using sw = new Stopwatch('part two');
+    console.log('What are the new total winnings?', part_two(games));
+}
+
 
 function part_one(games: Game[]): number {
     /**
@@ -67,6 +118,32 @@ function part_one(games: Game[]): number {
 
     let sum = 0;
     for(let i = 0; i < games.length; i++) {
+        log({
+            cards: games[i].cards.join(''),
+            type: ['high card', 'one pair', 'two pair', 'three of a kind', 'full house', 'four of a kind', 'five of a kind'][rank(games[i])],
+        })
+        sum += (i + 1) * games[i].bid;
+    }
+
+    return sum;
+}
+
+function part_two(games: Game[]): number {
+    /**
+     * We can reuse most of the code from part 1; the difference is that J can now be anything.
+     * In the code to determine the type of hand I now also keep track of the number of occurrences
+     * of the wildcard. Per type of hand can we than determine, using that number of occurrences,
+     * how to make optimal use of the wildcard(s). When sorting the hands, also consider `J` as 0.
+     */
+    games.sort((one, another) => one.compareToPartTwo(another));
+
+    let sum = 0;
+    for(let i = 0; i < games.length; i++) {
+        if (games[i].cards.includes('J'))
+            log({
+                cards: games[i].cards.join(''),
+                type: ['high card', 'one pair', 'two pair', 'three of a kind', 'full house', 'four of a kind', 'five of a kind'][rank_wildcard(games[i])],
+            })
         sum += (i + 1) * games[i].bid;
     }
 
@@ -88,20 +165,6 @@ function log(...args: unknown[]): void {
         console.log(...args);
     }
 }
-
-type Rank = {
-    readonly rank: number;
-    readonly cards?: number[],
-
-    readonly five_of_a_kind?: number,   // rank: 6
-    readonly four_of_a_kind?: number,   // rank: 5
-    readonly full_house?: number[],     // rank: 4
-    readonly three_of_a_kind?: number,  // rank: 3
-    readonly two_pair?: number[],       // rank: 2
-    readonly one_pair?: number,         // rank: 1
-
-    readonly remainder?: number[],
-};
 
 function rank(game: Game): number {
     /**
@@ -157,4 +220,126 @@ function rank(game: Game): number {
 
     // high card
     return 0;
+}
+
+function rank_wildcard(game: Game): number {
+    /**
+     * Determine a rank. The rank is an object that instantly tells you what the hand represents -
+     * five of a kind, four of a kind, etc -, the value of that sequence. And it has a remainder,
+     * which are all the remaining cards not part of the sequence.
+     *
+     * The ranks are cached so speed up the process.
+     */
+
+    const cards = game.cards;
+    const wildcard = 'J';
+
+    // count chars:
+    const per_char: Record<string, number> = {};
+    let num_of_wildcards = 0;
+    for (const char of cards) {
+        if (char === wildcard || false) {
+            num_of_wildcards++;
+        }
+        per_char[char] = (per_char[char] || 0) + 1;
+    }
+
+    const prepared = Object.entries(per_char).map(([card, count]) => ({ card, count }));
+    prepared.sort((one, another) => another.count !== one.count
+        ? another.count - one.count
+        : 0);
+
+    // has five of a kind?
+    if (prepared.length === 1) {
+        return Type.FIVE_OF_A_KIND;
+    }
+
+    // four of a kind?
+    if (prepared.length === 2 && prepared[0].count === 4) {
+        // AAAAK
+        // AAAAJ
+        // JJJJA
+
+        // Can we improve with the wildcard?
+        if (
+            num_of_wildcards === 1
+            || num_of_wildcards === 4
+        ) {
+            return Type.FIVE_OF_A_KIND;
+        }
+
+        return Type.FOUR_OF_A_KIND;
+    }
+
+    // full house?
+    if (prepared.length === 2 && prepared[0].count === 3 && prepared[1].count === 2) {
+        // AAAKK
+        // AAAJJ
+        // JJJAA
+
+        if (
+            (num_of_wildcards === 2 && prepared[0].card !== wildcard)    // AAAJJ
+            || (num_of_wildcards === 3 && prepared[1].card !== wildcard) // JJJAA
+        ) {
+            return Type.FIVE_OF_A_KIND;
+        }
+        return Type.FULL_HOUSE;
+    }
+
+    // three of a kind?
+    if (prepared.length === 3  && prepared[0].count === 3) {
+        // AAAKQ
+        // AAAJK
+        // JJJAK
+
+        // Can we improve with the wildcard?
+        if (num_of_wildcards === 1      // AAAJK
+            || num_of_wildcards === 3   // JJJAK
+        ) {
+            return Type.FOUR_OF_A_KIND;
+        }
+
+        return Type.THREE_OF_A_KIND;
+    }
+
+    // two pair?
+    if (prepared.length === 3 && prepared[0].count === 2) {
+        // AAKKQ
+        // AAKKJ
+        // AAJJK
+
+        if (num_of_wildcards === 2) {   // AAJJK
+            return Type.FOUR_OF_A_KIND;
+        }
+        if (num_of_wildcards === 1) {   // AAKKJ
+            return Type.FULL_HOUSE
+        }
+
+        return Type.TWO_PAIR;
+    }
+
+    // one pair?
+    if (prepared.length === 4) {
+        // AAKQT
+        // AAKQJ
+        // JJAKQ
+
+        if (num_of_wildcards === 2) {   // JJAKQ
+            return Type.THREE_OF_A_KIND;
+        }
+        if (num_of_wildcards === 1) {   // AKQQJ
+            return Type.THREE_OF_A_KIND;
+        }
+
+        return Type.ONE_PAIR;
+    }
+
+    // high card
+    // AKQT9
+    // AKQJT
+    if (num_of_wildcards === 1) {      // AKQJT
+        return Type.ONE_PAIR;
+    }
+
+    return Type.HIGH_CARD;
 }
