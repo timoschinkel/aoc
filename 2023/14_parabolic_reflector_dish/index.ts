fd94b80fe1b767c5b233181bafeba204343abbc6
Timo Schinkel <timo.schinkel@coolblue.nl>
2023-12-14 13:09:05 +0100

    [2023] Day 14, part 2

--- 2023/14_parabolic_reflector_dish/index.ts
+++ 2023/14_parabolic_reflector_dish/index.ts
@@ -8,6 +8,7 @@ const panel = parse(input);
 const Rock = {
     Squared: '#',
     Rounded: 'O',
+    Empty: '.',
 }
 
 {
@@ -17,7 +18,7 @@ const Rock = {
 
 {
     using sw = new Stopwatch('part two');
-    console.log('What are the new total winnings?', part_two());
+    console.log('What is the total load on the north support beams?', part_two(panel));
 }
 
 
@@ -48,21 +49,177 @@ function part_one(panel: ControlPanel): number {
     return load;
 }
 
-function part_two(): number {
-    return 0;
+function part_two(panel: ControlPanel): number {
+    /**
+     * This assignment reminds me of the Tetris-like puzzel from day 17 of 2022;
+     * it is not feasible to run the cycles so often. There probably is a pattern
+     * that repeats itself every x cycles.
+     *
+     * For every cycle I store the controlpanel after tilting, and store that in
+     * an array. After every cycle I check if the pattern is present in this array,
+     * and if that's the case then we have found our pattern length. With this we
+     * can calculate the number of cycles that are remaining, execute them and
+     * calculate the load after that last iteration.
+     */
+
+    const tilt_north = (): void => {
+        for(let col = 0; col < panel.width; col++) {
+            for (let row = 0; row < panel.height; row++) {
+                if (panel.rocks[row][col] === Rock.Squared) {
+                    continue; // we don't have to do anything
+                }
+
+                else if (panel.rocks[row][col] === Rock.Empty) {
+                    // find next Rock.Round and switch
+                    for(let r = row + 1; r < panel.height; r++) {
+                        if (panel.rocks[r][col] === Rock.Squared) {
+                            // No rock, update row
+                            row = r;
+                            break;
+                        }
+                        if (panel.rocks[r][col] === Rock.Rounded) {
+                            // switch and continue
+                            panel.rocks[row][col] = Rock.Rounded;
+                            panel.rocks[r][col] = Rock.Empty;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    const tilt_east = (): void => {
+        for(let row = panel.height - 1; row >= 0; row--) {
+            for (let col = panel.width - 1; col >= 0; col--) {
+                if (panel.rocks[row][col] === Rock.Squared) {
+                    continue; // we don't have to do anything
+                }
+
+                else if (panel.rocks[row][col] === Rock.Empty) {
+                    // find next Rock.Round and switch
+                    for(let c = col - 1; c >= 0; c--) {
+                        if (panel.rocks[row][c] === Rock.Squared) {
+                            // No rock, update row
+                            col = c;
+                            break;
+                        }
+                        if (panel.rocks[row][c] === Rock.Rounded) {
+                            // switch and continue
+                            panel.rocks[row][col] = Rock.Rounded;
+                            panel.rocks[row][c] = Rock.Empty;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    const tilt_south = (): void => {
+        for(let col = panel.width - 1; col >= 0; col--) {
+            for (let row = panel.height - 1; row >= 0; row--) {
+                if (panel.rocks[row][col] === Rock.Squared) {
+                    continue; // we don't have to do anything
+                }
+
+                else if (panel.rocks[row][col] === Rock.Empty) {
+                    // find next Rock.Round and switch
+                    for(let r = row - 1; r >= 0; r--) {
+                        if (panel.rocks[r][col] === Rock.Squared) {
+                            // No rock, update row
+                            row = r;
+                            break;
+                        }
+                        if (panel.rocks[r][col] === Rock.Rounded) {
+                            // switch and continue
+                            panel.rocks[row][col] = Rock.Rounded;
+                            panel.rocks[r][col] = Rock.Empty;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    const tilt_west = (): void => {
+        for(let row = 0; row < panel.height; row++) {
+            for (let col = 0; col < panel.width; col++) {
+                if (panel.rocks[row][col] === Rock.Squared) {
+                    continue; // we don't have to do anything
+                }
+
+                else if (panel.rocks[row][col] === Rock.Empty) {
+                    // find next Rock.Round and switch
+                    for(let c = col + 1; c < panel.width; c++) {
+                        if (panel.rocks[row][c] === Rock.Squared) {
+                            // No rock, update row
+                            col = c;
+                            break;
+                        }
+                        if (panel.rocks[row][c] === Rock.Rounded) {
+                            // switch and continue
+                            panel.rocks[row][col] = Rock.Rounded;
+                            panel.rocks[row][c] = Rock.Empty;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    const load = (panel: ControlPanel): number => {
+        let load = 0;
+
+        for(let col = 0; col < panel.width; col++) {
+            for(let row = 0; row < panel.height; row++) {
+                if (panel.rocks[row][col] === Rock.Rounded) {
+                    load += panel.height - row;
+                }
+            }
+        }
+
+        return load;
+    }
+
+    const patterns: string[] = [];
+
+    const num_of_cycles = 1000000000;
+    for (let cycle = 0; cycle < num_of_cycles; cycle++) {
+
+        tilt_north();
+        tilt_west();
+        tilt_south();
+        tilt_east();
+
+        // find a pattern
+        const pattern = panel.rocks.map(row => row.join('')).join('');
+        if (patterns.includes(pattern)) {
+            log('Found a pattern at cycle', cycle, 'matches cycle', patterns.indexOf(pattern), 'for frequency', cycle - patterns.indexOf(pattern));
+            const remaining = (num_of_cycles - patterns.indexOf(pattern)) % (cycle - patterns.indexOf(pattern));
+
+            cycle = num_of_cycles - remaining;
+            continue;
+        }
+        patterns.push(pattern);
+    }
+
+    return load(panel);
 }
 
 type ControlPanel = {
     readonly width: number;
     readonly height: number;
-    rocks: string[];
+    rocks: string[][];
 }
 
 function parse(input: string[]): ControlPanel {
     return {
         height: input.length,
         width: input[0].length,
-        rocks: input,
+        rocks: input.map(row => row.split('')),
     };
 }
 
@@ -70,4 +227,8 @@ function log(...args: unknown[]): void {
     if (debug) {
         console.log(...args);
     }
+}
+
+function print(panel: ControlPanel): void {
+    panel.rocks.forEach(row => console.log(row.join('')));
 }
