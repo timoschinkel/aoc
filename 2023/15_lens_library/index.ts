9bebb73d04051f408513f134df10e61cd925ec9c
Timo Schinkel <timo.schinkel@coolblue.nl>
2023-12-15 10:26:26 +0100

    [2023] Day 15, part 2

--- 2023/15_lens_library/index.ts
+++ 2023/15_lens_library/index.ts
@@ -13,7 +13,7 @@ const codes = input.split(',');
 
 {
     using sw = new Stopwatch('part two');
-    console.log('What are the new total winnings?', part_two());
+    console.log('What are the new total winnings?', part_two(codes));
 }
 
 
@@ -24,28 +24,71 @@ function part_one(codes: string[]): number {
     let sum = 0;
 
     for(const code of codes) {
-        let current = 0;
-        for(let i = 0; i < code.length; i++) {
-            current += code.charCodeAt(i);
-            current *= 17;
-            current %= 256;
-        }
-        sum += current;
+        sum += hash(code);
     }
 
     return sum;
 }
 
-function part_two(): number {
-    return 0;
+function part_two(codes: string[]): number {
+    /**
+     * Mostly administration; important is that we keep the order of
+     * items in the boxes. Luckily I'm using JavaScript and not Python
+     * and is the dictionary stable.
+     */
+    const boxes: Record<string, number>[] = new Array(256).fill({});
+
+    for (const code of codes) {
+        const { groups: {label, op, target } } = code.match(/^(?<label>[^=-]+)(?<op>(=|-))(?<target>.+)?$/si);
+
+        const box = hash(label);
+        if (op === '=') {
+            // target is the focal strength
+            boxes[box] = { ...boxes[box], [label]: parseInt(target) };
+        } else if (op === '-') {
+            delete boxes[box][label];
+        }
+
+        log(`After "${code}"`);
+        print(boxes);
+        log('');
+    }
+
+    // calculate focal length
+    let sum = 0;
+    for (let b = 0; b < boxes.length; b++) {
+        const lenses = Object.entries(boxes[b]);
+        for(let slot = 0; slot < lenses.length; slot++) {
+            const power = (b + 1) * (slot + 1) * lenses[slot][1];
+            log(lenses[slot][0], power);
+            sum += power;
+        }
+    }
+    return sum;
 }
 
-function parse(input: string[]): unknown {
-    return null;
+function hash(code: string): number {
+    let current = 0;
+    for(let i = 0; i < code.length; i++) {
+        current += code.charCodeAt(i);
+        current *= 17;
+        current %= 256;
+    }
+    return current;
 }
 
 function log(...args: unknown[]): void {
     if (debug) {
         console.log(...args);
+    }
+}
+
+function print(boxes: Record<string, number>[]): void {
+    if (!debug) return;
+
+    for (let b = 0; b < boxes.length; b++) {
+        const lenses = Object.entries(boxes[b]);
+        if (lenses.length === 0) continue;
+        log(`Box ${b}: ${lenses.map(([key, value]) => `[${key} ${value}]`).join(' ')}`);
     }
 }
