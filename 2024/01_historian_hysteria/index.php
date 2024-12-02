928bf82a5ce607dee4ac859142e207c63bf898f8
Timo Schinkel <timo.schinkel@coolblue.nl>
2024-12-02 09:00:16 +0100

    [2024] Day 1

---
+++ 2024/01_historian_hysteria/index.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+$input = __DIR__ . DIRECTORY_SEPARATOR . ($argv[1] ?? 'example') . '.txt';
+
+$rows = array_filter(explode(PHP_EOL, file_get_contents($input)));
+
+$one = [];
+$two = [];
+foreach ($rows as $row) {
+    [$left, $right] = preg_split('%\s+%', $row, 2);
+    $one[] = $left;
+    $two[] = $right;
+}
+
+function part_one(array $one, array $two): int {
+    sort($one);
+    sort($two);
+
+    $difference = 0;
+    for ($i = 0; $i < count($one); $i++) {
+        $difference += abs($one[$i] - $two[$i]);
+    }
+
+    return $difference;
+}
+
+echo 'What is the total distance between your lists? ' . part_one($one, $two) . PHP_EOL;
+
+function part_two(array $one, array $two): int {
+    $counts = array_count_values($two); // hail PHP!
+
+    $score = 0;
+    foreach ($one as $id) {
+        $score += $id * ($counts[$id] ?? 0);
+    }
+
+    return $score;
+}
+
+echo 'What is their similarity score? ' . part_two($one, $two) . PHP_EOL;
