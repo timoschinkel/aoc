7856bb99b3ae70747ea86c2d4b8840c4c7947ba6
Timo Schinkel <timo.schinkel@coolblue.nl>
2024-12-03 08:45:28 +0100

    [2024] Day 2 optimization

--- 2024/02_red-nosed_reports/index.php
+++ 2024/02_red-nosed_reports/index.php
@@ -36,8 +36,18 @@ function part_two(array $reports): int {
     return count(array_filter($reports, fn(array $report): bool => is_safe($report) || is_safe_with_dampener($report)));
 }
 
+/*
+ * `part_two()` takes 2ms, that can be done faster. Instead of exhaustive trying all possibilities, we can find the
+ * level that makes the report unsafe. Now there are three possible ways to solve this; remove previous level, remove
+ * current level or remove next level. This reduces the number of possibilities per report to 2 (when the unsafeness
+ * occurs already between the first and second level) or 3.
+ */
+function part_two_optimized(array $reports): int {
+    return count(array_filter($reports, fn(array $report): bool => is_safe_with_dampener_optimized($report)));
+}
+
 $sw->start();
-echo 'How many reports are now safe? ' . part_two($reports) . ' (' . $sw->ellapsedMS() . 'ms)' . PHP_EOL;
+echo 'How many reports are now safe? ' . part_two_optimized($reports) . ' (' . $sw->ellapsedMS() . 'ms)' . PHP_EOL;
 
 function is_safe(array $report): bool {
     $prev = $report[1] - $report[0];
@@ -57,7 +67,8 @@ function is_safe(array $report): bool {
     return true;
 }
 
-function is_safe_with_dampener(array $report): bool {
+function is_safe_with_dampener(array $report): bool
+{
     // just try all permutations
     for ($i = 0; $i < count($report); $i++) {
         $mutation = [...array_slice($report, 0, $i), ...array_slice($report, $i + 1)];
@@ -65,6 +76,35 @@ function is_safe_with_dampener(array $report): bool {
             return true;
         }
     }
+}
 
-    return false;
+function is_safe_with_dampener_optimized(array $report): bool {
+    $prev = $report[1] - $report[0];
+
+    // Don't forget to check the first difference
+    if ($prev === 0 || $prev < -3 || $prev > 3) {
+        return is_safe(remove_index($report, 0)) || is_safe(remove_index($report, 1));
+    };
+
+    for ($i = 1; $i < count($report) - 1; $i++) {
+        $diff = $report[$i + 1] - $report[$i];
+
+        if ($diff < -3 || $diff > 3 || $diff === 0 || ($prev < 0 && $diff > 0) || ($prev > 0 && $diff < 0)) {
+            return is_safe(remove_index($report, $i - 1))
+                || is_safe(remove_index($report, $i))
+                || is_safe(remove_index($report, $i + 1));
+        }
+
+        $prev = $diff;
+    }
+
+    return true;
+}
+
+function remove_index(array $arr, int $index): array
+{
+    $clone = [...$arr];
+    unset($clone[$index]);
+
+    return array_values($clone);
 }
