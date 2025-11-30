308a499c6d7f32cf4f7fcf5d11f5190f874ec2ce
Timo Schinkel <timo.schinkel@coolblue.nl>
2025-11-30 18:09:04 +0100

    2025] Introduce a boilerplate

---
+++ 2025/00-Boilerplate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+string[] input = File.ReadAllLines(Environment.GetEnvironmentVariable("INPUT") != null ? $"./{Environment.GetEnvironmentVariable("INPUT")}" : "./input.txt");
+
+// Parse input
+
+// Part 01
+var sw = Stopwatch.StartNew();
+// perform calculation
+sw.Stop();
+PrintElapsedTime($"The answer is: {0} ([ELAPSED])", sw.Elapsed);
+
+// Part 02
+sw = Stopwatch.StartNew();
+// perform calculation
+sw.Stop();
+PrintElapsedTime($"The answer is: {0} ([ELAPSED])", sw.Elapsed);
+
+void PrintElapsedTime(string message, TimeSpan ts)
+{
+    var ns = ts.TotalNanoseconds;
+    var parts = message.Split("[ELAPSED]");
+    for (var i = 0; i < parts.Length; i++)
+    {
+        Console.Write(parts[i]);
+        if (i < parts.Length - 1)
+        {
+            if (ns > 1000000000)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"{Math.Round(ns / 1000000000, 2)}s");
+            }
+            else if (ns > 1000000)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"{Math.Ceiling(ns / 1000000)}ms");
+            }
+            else if (ns > 1000)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"{Math.Ceiling(ns / 1000)}μs");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"{ns}ns");
+            }
+            
+            Console.ResetColor();
+        }
+    }
+    
+    Console.WriteLine("");
+}
---
+++ 2025/README.md
@@ -0,0 +1,11 @@
+# Advent of Code 2025
+
+See: https://adventofcode.com/2025
+
+## Usage
+This year's Advent of Code is written using C# 8.0 - because that's the version I have installed at this moment.
+
+Everyday is a separate project with `Program.cs`. This is a console application that will look for the file `./input.txt`. If you want to run with a different input - e.g. `example.txt` - then this can be specified using the `INPUT` environment variable. The solution has build configurations for both `input.txt` and `example.txt`.
+
+## Requirements
+- .NET 8.0
