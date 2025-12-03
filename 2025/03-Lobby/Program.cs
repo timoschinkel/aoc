ef40751ee374af54f444475dff72cfa23dedbfcc
Timo Schinkel <timo.schinkel@coolblue.nl>
2025-12-03 09:09:03 +0100

    [2025] Day 03, part 02

--- 2025/03-Lobby/Program.cs
+++ 2025/03-Lobby/Program.cs
@@ -45,7 +45,46 @@ long CalculateLargestJoltage(int[] bank)
 
 long PartTwo(IEnumerable<int[]> banks)
 {
-    return 0;
+    long joltage = 0;
+    foreach (var bank in banks)
+    {
+        joltage += CalculateNewLargestJoltage(bank, 12);
+    }
+    return joltage;
+}
+
+int FindMaxWithinRange(int[] bank, int lower, int upper)
+{
+    int maxValue = bank[lower];
+    if (maxValue == 9) return lower; // No higher value possible
+
+    int maxIndex = lower;
+    for (int index = lower + 1; index <= upper; index++)
+    {
+        if (bank[index] == 9) return index;
+        
+        if (bank[index] > maxValue)
+        {
+            maxValue = bank[index];
+            maxIndex = index;
+        }
+    }
+    return maxIndex;
+}
+
+long CalculateNewLargestJoltage(int[] bank, int size)
+{
+    long max = 0;
+
+    // Find the largest in the search space
+    int position = -1;
+    for (int i = 0; i < size; i++)
+    {
+        position = FindMaxWithinRange(bank, position + 1, bank.Length - size + i);
+        max += bank[position] * (long)Math.Pow(10, size - i - 1);
+    }
+
+    return max;
 }
 
 void PrintElapsedTime(string message, TimeSpan ts)
